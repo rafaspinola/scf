49d6397948809d54c76f7b793cc81f004a0176fb
Rafael Spinola <rafael.spinola@gmail.com>
2015-07-02 22:51:36 -0300

    GitHub

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,17 @@
 set :stage, :production
 
+set :repository, "file:///vagrant/projetos/ruby/scf"
+set :local_repository, "file:///vagrant/projetos/ruby/scf"
+set :deploy_to, "/home/railsapps/scf_dale"
+
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{railsapps@keterconsultoria.com.br}
+#role :web, %w{railsapps@keterconsultoria.com.br}
+#role :db,  %w{railsapps@keterconsultoria.com.br}
 
 # Extended Server Syntax
 # ======================
@@ -15,14 +19,16 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'keterconsultoria.com.br', user: 'railsapps', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+  set :ssh_options, {
+    port: 7722
+    #keys: %w(/Users/rafaelspinola/.ssh/id_rsa)
+  }
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
