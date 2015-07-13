2e42cffde793d6f07b586e0831ad4cba426ae127
Rafael Spinola <rafael.spinola@gmail.com>
2015-07-13 15:57:21 -0300

    exibir contas por ordem de pai/filho

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,23 @@
 class Account < ActiveRecord::Base
   belongs_to :top_account, class_name: "Account", foreign_key: "top_account_id"
+  has_many :child_accounts, class_name: "Account", foreign_key: "top_account_id"
 
   def to_s
     self.description
   end
+
+  def self.ordered_list
+    root = root_accounts
+    tree = []
+    root.each do |r|
+      tree << r
+      tree << r.child_accounts
+    end
+    tree.flatten
+  end
+
+  def self.root_accounts
+    self.where top_account: nil
+  end
+
 end
