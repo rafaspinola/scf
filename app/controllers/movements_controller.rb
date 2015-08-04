6500f24bb0261a4412af993923d62a5fe58dba73
Rafael Spinola <rafael.spinola@gmail.com>
2015-08-03 23:08:45 -0300

    Marcar movimentos como para enviar para a contabilidade

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -49,6 +49,6 @@ class MovementsController < ApplicationController
     end
 
     def movement_params
-      params.require(:movement).permit(:due_date, :description, :value, :course_class_id, :account_id, :result_center_id, :bank_id, :transfer, :to_bank_id, :credit)
+      params.require(:movement).permit(:due_date, :description, :value, :course_class_id, :account_id, :result_center_id, :bank_id, :transfer, :to_bank_id, :credit, :accountable)
     end
 end
