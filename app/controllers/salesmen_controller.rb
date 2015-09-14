d8edf43b313435039b3a321ebf0619b549e6aa1b
Rafael Spinola <rafael.spinola@gmail.com>
2015-09-13 23:02:25 -0300

    salvar identificador do vendedor

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -43,6 +43,6 @@ class SalesmenController < ApplicationController
     end
 
     def salesman_params
-      params.require(:salesman).permit(:name, :bank, :agency, :account, :operation)
+      params.require(:salesman).permit(:name, :bank, :agency, :account, :operation, :identifier)
     end
 end
